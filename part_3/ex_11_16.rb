c26a04d5aac80c617e62133086dfa101d8413f9c
alexfmjpn <lexfnjpm@mail.ru>
2020-01-26 01:47:12 +0000

    Added ex_11_16.rb

---
+++ part_3/ex_11_16.rb
@@ -0,0 +1,28 @@
+
+# simple array
+languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
+languages.each{|language| puts language}
+
+# each on it's own line
+# HTML
+# CSS
+# JavaScript
+# Python
+# Ruby
+
+
+# Process exited with code: 0
+#  with print 
+
+languages = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
+languages.each{|language| print language}
+
+# all in one last line 
+# HTML
+# CSS
+# JavaScript
+# Python
+# Ruby
+# HTMLCSSJavaScriptPythonRuby
+
+# Process exited with code: 0
