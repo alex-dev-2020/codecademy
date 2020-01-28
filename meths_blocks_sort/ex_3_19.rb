bcf80412957fa1b4da6cbad85e2590a83850a47a
Alex Dev <alex_dev_2020@mail.ru>
2020-01-29 00:21:43 +1000

    Created ex_3_19.rb

---
+++ meths_blocks_sort/ex_3_19.rb
@@ -0,0 +1,10 @@
+# Define your method below!
+def greeting (name)
+  puts "Hi #{name}"
+  # Define your method above this line.
+end
+greeting ("Alex Dev")# Ignore this for now. We'll explain
+         # it in the next exercise!
+
+# запилил собственный метод
+# Hi Alex Dev
