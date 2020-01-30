7248fe11d79a1480c65b08be32cca7db9422b496
alexfmjpn <lexfnjpm@mail.ru>
2020-01-30 22:52:04 +0000

    Added code & tested ex_2_10.rb

--- part_6_1/ex_2_10.rb
+++ part_6_1/ex_2_10.rb
@@ -1,2 +1,18 @@
 # A NIGHT AT THE MOVIES
 # Setting Up
+movies = {
+  memento: 3,
+  primer: 3.5,
+  the_matrix: 3,
+  truman_show: 4,
+  red_dawn: 1.5,
+  skyfall: 4,
+  alex_cross: 2,
+  uhf: 1,
+  lion_king: 3.5
+}
+puts "Please input your choice:"
+choice = gets.chomp
+
+# Please input your choice:
+# the_matrix
