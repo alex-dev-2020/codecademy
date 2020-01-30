388f1ab0e229b97e2b99fd16e2017825330ce94a
alexfmjpn <lexfnjpm@mail.ru>
2020-01-30 02:38:29 +0000

    Created & tested ex_9_15.rb

---
+++ part_6/ex_9_15.rb
@@ -0,0 +1,28 @@
+# 1. We have an array of strings we’d like to later use as hash keys, but we’d rather they be symbols.
+
+# 2. Create a new variable, symbols, and store an empty array in it.
+# 3. Use .each to iterate over the strings array.
+# 4. For each s in strings, use .to_sym to convert s to a symbol and use .push to add that new symbol to symbols.
+# 5. Print the symbols array.
+
+strings = ["HTML", "CSS", "JavaScript", "Python", "Ruby"]
+
+# Add your code below!
+# вариант с использованием <<
+symbols = []
+strings.each do |s| 
+  symbols << s.to_sym
+end
+# Add your code below!
+print symbols
+# [:HTML, :CSS, :JavaScript, :Python, :Ruby]
+# вариант с использованием .push
+
+symbols = []
+strings.each do |s| 
+  sym =s.to_sym
+  symbols.push(sym)
+end
+# Add your code below!
+print symbols
+# [:HTML, :CSS, :JavaScript, :Python, :Ruby]
