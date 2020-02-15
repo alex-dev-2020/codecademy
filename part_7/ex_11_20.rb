49a5c3d6948f448439ee1e0f74537c453ff0fbb6
alexfmjpn <lexfnjpm@mail.ru>
2020-02-15 15:47:46 +0000

    Added & checked ex_11_20.rb

---
+++ part_7/ex_11_20.rb
@@ -0,0 +1,22 @@
+# Use .upto to puts the capital letters "L" through "P".
+# (Make sure to use puts and not print, so each letter is on its own line!)
+
+
+# my solution
+
+"L".upto("P").each{|letter| puts letter} 
+
+
+# solution from  Codecademy
+
+"L".upto("P"){|letter| puts letter} 
+
+# L
+# M
+# N
+# O
+# P
+
+# Process exited with code: 0
+
+# both outputs are equal
